4a3bb9acd85aa16388c0ed139b32e3011349516f
Farid AMOUSSA <amoussa.farid2@gmail.com>
2023-07-02 21:44:23 +0100

    dequeue style

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 11);
 
 function theme_enqueue_styles() {
-    // wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    if (is_front_page()) {
+        wp_dequeue_style( 'wp-block-library' );
+        wp_dequeue_style( 'twenty-twenty-one-style' );
+        wp_dequeue_style( 'twenty-twenty-one-print-style' );
+    }
 
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.css'));
 }
